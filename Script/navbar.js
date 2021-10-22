4de6f7623b754f2258e98ba08d38d2201d3ad187
Suraj Karosia <53578095+1998Suraj@users.noreply.github.com>
2021-10-22 20:37:11 +0530

    Create navbar.js

---
+++ Script/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+    return ` <h3><a href="Food_App.html">Home</a></h3>
+<div id="options">
+    <h3><a href="latest_receipe.html">Latest Recepie</a></h3>
+    <h3><a href="receipe_of_the_day.html">Receipe Of The Day</a></h3>
+    <h3><a href="#">Sign In</a></h3>
+    <h3><a href="#">Sign Up</a></h3>
+</div>`
+}
+
+export default navbar;
